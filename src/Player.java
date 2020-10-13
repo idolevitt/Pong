717741c637c2fb336f0bac6c3c63ae6985750d17
idolevitt <idolevit2@gmail.com>
2020-10-13 12:38:02 +0300

    Bug fix
    Handling exeptions and adding comments

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -26,21 +29,40 @@ public class Player extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) {
 
 
         scn = new Scanner(System.in);
-
-        socket = new Socket(IP, PORT);
-        dos = new DataOutputStream(socket.getOutputStream());
+        //establishing connection to the server:
+        while(true) {
+            try {
+                socket = new Socket(IP, PORT);
+                dos = new DataOutputStream(socket.getOutputStream());
+                break;
+            } catch (IOException i) {
+            }
+        }
 
         input = new ArrayList<>();
 
-        //TODO: make new scene for key press handler
-        Scene scene = new Scene(new Group(new Canvas(100,100)));
+        Scene scene = new Scene(new Group(new Canvas(200,0)));
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                try {
+                    System.out.println("Closing connection");
+                    dos.writeUTF("Close");
+                    dos.close();
+                    socket.close();
+                    Platform.exit();
+                    System.exit(0);
+                } catch (IOException e) {}
+            }
+        });
+        //Player pressed a key
         scene.setOnKeyPressed(e -> {
             String command = e.getCode().toString();
             System.out.println(e.getCode().toString() + " was pressed");
@@ -53,7 +75,7 @@ public class Player extends Application {
                 }
             }
         });
-
+        //Player released a key
         scene.setOnKeyReleased(e -> {
             String command = e.getCode().toString();
             if(input.contains(command)) {
--- src/Window.java
+++ src/Window.java
@@ -1,11 +1,17 @@
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.DataInputStream;
 import java.io.IOException;
@@ -21,16 +27,18 @@ public class Window extends Application{
     private final int windowHeight = 600;
 
     //Players properties:
-    private int playerWidth = 10;
-    private int playerHeight = 90;
+    private final int playerWidth = 10;
+    private final int playerHeight = 90;
 
     //Player 1:
     private int playerOneXPos = 30;
     private int playerOneYPos = 30;
+    private int scorePlayerOne;
 
     //Player 2:
     private int playerTwoXPos = windowWidth - 30;
     private int playerTwoYPos = 300;
+    private int scorePlayerTwo;
 
     //Ball properties:
     private final int ballRadius = 20;
@@ -48,6 +56,8 @@ public class Window extends Application{
     private ServerSocket server;
     private Socket socketPlayerOne;
     private Socket socketPlayerTwo;
+    private DataInputStream disOne;
+    private DataInputStream disTwo;
 
 
     public static void main(String[] args) {
@@ -59,56 +69,83 @@ public class Window extends Application{
 
         input = new ArrayList<>();
 
-        //Establishing sockets:
+        scorePlayerOne = 0;
+        scorePlayerTwo = 0;
+
+        //Establishing connections:
 
         server = new ServerSocket(9000);
-        /*
+
         System.out.println("waiting for clients...");
+
+        //Player 1:
         socketPlayerOne = server.accept();
         System.out.println("Player 1 connected");
-        DataInputStream disOne = new DataInputStream(socketPlayerOne.getInputStream());
+        disOne = new DataInputStream(socketPlayerOne.getInputStream());
         Thread playerOne = new Thread(() -> {
             while (true) {
                 try {
                     String key = disOne.readUTF() + "P1";
                     System.out.print("adding " + key + " to the input list");
-                    if (key.charAt(0) == 'R') {
+                    //"Close command":
+                    if(key.equals("CloseP1")){
+                        try{
+                            disOne.close();
+                            socketPlayerOne.close();
+                            System.out.println("Player 1 disconnected");
+                            break;
+                        }catch (IOException i){
+                            i.printStackTrace();
+                        }
+                    }
+                    //Key released:
+                    else if (key.charAt(0) == 'R') {
                         System.out.println("Removing " + key + " from the list");
                         input.remove(key.substring(1));
                     }
+                    //Key pressed:
                     else {
                         System.out.println("Adding " + key + " to the list");
                         input.add(key);
                     }
-                } catch (IOException i) {
-                    i.printStackTrace();
-                }
+                } catch (IOException i) {}
             }
         });
         playerOne.start();
 
-         */
 
+        //Player 2:
         System.out.println("waiting for player 2:");
         socketPlayerTwo = server.accept();
         System.out.println("Player 2 connected");
-        DataInputStream disTwo = new DataInputStream(socketPlayerTwo.getInputStream());
+        disTwo = new DataInputStream(socketPlayerTwo.getInputStream());
         Thread playerTwo = new Thread(() -> {
 
             while (true) {
                 try {
                     String key = disTwo.readUTF() + "P2";
-                    if (key.charAt(0) == 'R') {
+                    //"Close command":
+                    if(key.equals("CloseP2")){
+                        try{
+                            disTwo.close();
+                            socketPlayerTwo.close();
+                            System.out.println("Player 2 disconnected");
+                            break;
+                        }catch (IOException i){
+                            i.printStackTrace();
+                        }
+                    }
+                    //Key released:
+                    else if (key.charAt(0) == 'R') {
                         System.out.println("Removing " + key + " from the list");
                         input.remove(key.substring(1));
                     }
+                    //Key pressed:
                     else {
                         System.out.println("Adding " + key + " to the list");
                         input.add(key);
                     }
-                } catch (IOException i) {
-                    i.printStackTrace();
-                }
+                } catch (IOException i) {}
             }
         });
         playerTwo.start();
@@ -130,6 +167,22 @@ public class Window extends Application{
 
         primaryStage.setScene(scene);
 
+        //close operation:
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                try {
+                    System.out.println("Closing game...");
+                    disOne.close();
+                    socketPlayerOne.close();
+                    //disTwo.close();
+                    //socketPlayerTwo.close();
+                    Platform.exit();
+                    System.exit(0);
+                } catch (IOException e) {}
+            }
+        });
+
         //Handling the user input
         //input = new ArrayList<>();
 
@@ -150,20 +203,34 @@ public class Window extends Application{
         {
             public void handle(long currentNanoTime) {
 
+                //Player 1 keys:
                 if (input.contains("UPP1") && playerTwoYPos > 0)
                     playerTwoYPos -= 5;
                 if (input.contains("DOWNP1") && playerTwoYPos < windowHeight - playerHeight)
                     playerTwoYPos += 5;
+                //Player 2 keys:
                 if (input.contains("UPP2") && playerOneYPos > 0)
                     playerOneYPos -= 5;
                 if (input.contains("DOWNP2") && playerOneYPos < windowHeight - playerHeight)
                     playerOneYPos += 5;
 
-                moveBall();
+                //Update the ball location and properties
+                try {
+                    moveBall();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
                 //Setting the background color to black:
                 gc.setFill(Color.BLACK);
                 gc.fillRect(0,0, canvas.getWidth(), canvas.getHeight());
+
+                //drawing score:
+                gc.setFill(Color.CYAN);
+                gc.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
+                //score.setFont(Font.font("Verdana", 50));
+                //score.setFill(Color.AZURE);
+                gc.fillText(scorePlayerTwo +" : " + scorePlayerOne, 400, 60);
 
                 gc.setFill(Color.WHITE);
                 //Drawing player 1:
@@ -178,19 +245,24 @@ public class Window extends Application{
 
     }
 
-    public void moveBall(){
-
+    public void moveBall() throws InterruptedException {
+        //Ball bumped the upper wall:
         if (ballYPos <= ballRadius && ballSpeedY < 0)
             ballSpeedY *= -1;
-
+        //Ball bumped the bottom wall:
         if (ballYPos >= windowHeight - ballRadius && ballSpeedY > 0)
             ballSpeedY *= -1;
-
-        if (ballXPos <= ballRadius || ballXPos >= windowWidth - ballRadius){
-            ballSpeedY = 0;
-            ballSpeedY = 0;
+        //Player 1 scored a goal:
+        if (ballXPos <= ballRadius){
+            scorePlayerOne++;
+            restartBall();
         }
-
+        //Player 2 scored a goal:
+        if (ballXPos >= windowWidth - ballRadius){
+            scorePlayerTwo++;
+            restartBall();
+        }
+        //The ball hit player 2:
         if (ballXPos - ballRadius <= playerOneXPos - playerWidth &&
                 ballXPos + ballRadius >= playerOneXPos - playerWidth - ballRadius &&
                 ballYPos >= playerOneYPos && ballYPos <= playerOneYPos + playerHeight) {
@@ -200,7 +272,7 @@ public class Window extends Application{
                 ballSpeedY *= 1.05;
             }
         }
-
+        //the ball hit player 1:
         if (ballXPos + ballRadius >= playerTwoXPos && ballXPos + ballRadius <= playerTwoXPos + ballRadius &&
                 ballYPos >= playerTwoYPos && ballYPos <= playerTwoYPos + playerHeight) {
             if (ballSpeedX > 0) {
@@ -209,11 +281,20 @@ public class Window extends Application{
                 ballSpeedY *= 1.05;
             }
         }
-
+        //updating ball location:
         ballXPos += ballSpeedX;
         ballYPos += ballSpeedY;
 
 
+    }
+
+    private void restartBall() throws InterruptedException {
+        ballXPos = windowWidth / 2;
+        ballYPos = windowHeight / 2;
+        ballSpeedX = 3;
+        ballSpeedY = 3;
+
+        TimeUnit.SECONDS.sleep(2);
     }
 
 }
